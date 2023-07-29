8076ab506f08085927299496fd85fcb44b354d7e
BeniaminAV <57075208+BeniaminAV@users.noreply.github.com>
2023-07-29 14:11:25 +0300

    Update candy.ts

--- server/src/controllers/candy.ts
+++ server/src/controllers/candy.ts
@@ -2,6 +2,7 @@ import express from "express"
 import Honey from "../model/honey"
 import Honeys from "../data/honey"
 
+// function for get all Candy
 export const getAllCandy = (req: express.Request, res: express.Response) => {
   try {
     const allCandy: Honey[] = Honeys.filter((honey) => honey.title === "Candy")
@@ -16,6 +17,7 @@ export const getAllCandy = (req: express.Request, res: express.Response) => {
   }
 }
 
+//function for get Candy by id
 export const getCandyById = (req: express.Request, res: express.Response) => {
   try {
     const id: number = parseInt(req.params.id)
